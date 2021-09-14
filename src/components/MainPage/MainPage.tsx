2241e91c46b9f47f3a3c1b2ec45d65639b828ac6
Cubaa <jakub29-11@wp.pl>
2021-09-14 14:49:17 +0200

    Use Effect fixes and small changes
    Use Effect fixes and small changes

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,129 +1,126 @@
-import React, {useEffect, useState} from 'react'
-import styled from 'styled-components'
-import {NavBar} from '../NavBar/NavBar'
-import {SearchAndContent} from '../SearchAndContent/SearchAndContent'
-import {useDispatch} from 'react-redux'
-import {useSelector} from 'react-redux'
-import {IState} from '../../reducers'
-import {ICountriesReducer} from '../../reducers/countriesReducers'
-import {getCountries} from '../../Actions/countriesActions'
-import {IDimensions} from '../../entities/dimensionsType'
-import {
-    BrowserRouter as Router,
-    Route
-  } from "react-router-dom";
-import { CountryDetails } from '../CountryDetails/CountryDetails'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { NavBar } from "../NavBar/NavBar";
+import { SearchAndContent } from "../SearchAndContent/SearchAndContent";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { IState } from "../../reducers";
+import { ICountriesReducer } from "../../reducers/countriesReducers";
+import { getCountries } from "../../Actions/countriesActions";
+import { IDimensions } from "../../entities/dimensionsType";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CountryDetails } from "../CountryDetails/CountryDetails";
 
+type GetCountries = ReturnType<typeof getCountries>;
 
-type GetCountries = ReturnType<typeof getCountries>
-
-interface IThemeColor{
-    themeColor: string;
-    switchThemeColor: () => void;
+interface IThemeColor {
+  themeColor: string;
+  switchThemeColor: () => void;
 }
 
+export const MainPage: React.FC<IThemeColor> = (props) => {
+  const themeColor = props.themeColor;
+  const switchThemeColor = props.switchThemeColor;
+  const [dimensions, setDimensions] = useState<IDimensions>({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  const [pagination, setPagination] = useState<string>("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch<GetCountries>(getCountries());
 
-export const MainPage:React.FC<IThemeColor> = (props)=>{
-    const themeColor = props.themeColor
-    const switchThemeColor = props.switchThemeColor
-    const [dimensions, setDimensions] = useState<IDimensions>({
+    return () => {};
+  }, [dispatch]);
+
+  const { countriesList } = useSelector<IState, ICountriesReducer>(
+    (globalState) => ({
+      ...globalState.countries,
+    })
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
         height: window.innerHeight,
-        width: window.innerWidth
-    })
-    const [pagination, setPagination] = useState<string>("")
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch<GetCountries>(getCountries())
-    }, [])
+        width: window.innerWidth,
+      });
+    }
+    window.addEventListener("resize", handleResize);
 
+    if (dimensions.width < 1024) {
+      setPagination("mobile");
+    } else setPagination("desktop");
 
-    const { countriesList } = useSelector<IState, ICountriesReducer >(globalState =>({
-        ...globalState.countries,
-       
-        
-    }))
-  
-    useEffect(()=>{
-        function handleResize(){
-            setDimensions({
-                height: window.innerHeight,
-                width: window.innerWidth
-                })
-        }
-        window.addEventListener("resize", handleResize)
-       
-        if(dimensions.width < 1024){
-            setPagination("mobile")
-        }else setPagination("desktop")
-        
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
-    })
-  
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [dimensions.width]);
 
-    const themeColorSettings = {themeColor, switchThemeColor}
-    const countriesListAndFiltered = {countriesList}
-    
-    return(
-        <>  
-        <Router>
-            <NavBarWrapper>
-                <NavBar {...themeColorSettings}/>
-            </NavBarWrapper>
-            <Route path='/' exact>
-            <SearchAndContentWrapper>
-                <SearchAndContent {...countriesListAndFiltered} pagination={pagination} {...themeColorSettings}/> 
-            </SearchAndContentWrapper>
-            </Route>
-            <Route path='/:region/:country'>
-                <CountryDetailsWrapper>
-                    <CountryDetails themeColor = {themeColor}/>
-                </CountryDetailsWrapper>
-            </Route>
-        </Router>
-        </>
-    )
-}
+  const themeColorSettings = { themeColor, switchThemeColor };
+  const countriesListAndFiltered = { countriesList };
+
+  return (
+    <>
+      <Router>
+        <NavBarWrapper>
+          <NavBar {...themeColorSettings} />
+        </NavBarWrapper>
+        <Route path="/" exact>
+          <SearchAndContentWrapper>
+            <SearchAndContent
+              {...countriesListAndFiltered}
+              pagination={pagination}
+              {...themeColorSettings}
+            />
+          </SearchAndContentWrapper>
+        </Route>
+        <Route path="/:region/:country">
+          <CountryDetailsWrapper>
+            <CountryDetails themeColor={themeColor} />
+          </CountryDetailsWrapper>
+        </Route>
+      </Router>
+    </>
+  );
+};
 
 const NavBarWrapper = styled.nav`
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
-display: flex;
-justify-content: space-between;
-width: 100%;
-background-color: ${props => props.theme.colors.navBarBackground};
-box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
-padding: 0.5rem 0;
-h3{
+  @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap");
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.navBarBackground};
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  padding: 0.5rem 0;
+  h3 {
     padding: 0.75rem 0 0 0.75rem;
-    font-family: 'Nunito Sans', sans-serif;
+    font-family: "Nunito Sans", sans-serif;
     font-weight: 800;
-    color: ${props => props.theme.colors.fontColor}
-}
-`
+    color: ${(props) => props.theme.colors.fontColor};
+  }
+`;
 const SearchAndContentWrapper = styled.section`
-padding-top: 1.5rem;
-width: 100%;
-display: flex;
-flex-direction: column;
-
-`
+  padding-top: 1.5rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const CountryDetailsWrapper = styled.section`
-@media (max-width: 768px){
+  @media (max-width: 768px) {
     display: flex;
     flex-direction: column;
     align-items: center;
-}
+  }
 
-width: 100%;
-padding-top: 3rem;
-min-height: 10vh;
-min-height: 5vh;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-padding-bottom: 1rem;
-
-`
+  width: 100%;
+  padding-top: 3rem;
+  min-height: 10vh;
+  min-height: 5vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 1rem;
+`;
--- src/components/SearchAndContent/SearchAndContent.tsx
+++ src/components/SearchAndContent/SearchAndContent.tsx
@@ -1,247 +1,258 @@
-import React, {useState, useEffect, useRef, createRef} from 'react'
-import styled from 'styled-components'
-import {SearchInput} from './SearchInput/SearchInput'
-import { SelectRegion } from './SelectRegion/SelectRegion'
-import {CountriesContent} from './CountriesContent/CountriesContent'
-import {ISingleCountry} from '../../entities/singleCountryType'
-import {Pagination} from './Pagination/Pagination'
-import {HiOutlineArrowCircleLeft, HiOutlineArrowCircleRight} from 'react-icons/hi'
+import React, { useState, useEffect, useRef, createRef } from "react";
+import styled from "styled-components";
+import { SearchInput } from "./SearchInput/SearchInput";
+import { SelectRegion } from "./SelectRegion/SelectRegion";
+import { CountriesContent } from "./CountriesContent/CountriesContent";
+import { ISingleCountry } from "../../entities/singleCountryType";
+import { Pagination } from "./Pagination/Pagination";
+import {
+  HiOutlineArrowCircleLeft,
+  HiOutlineArrowCircleRight,
+} from "react-icons/hi";
 import { useHistory } from "react-router-dom";
 
-interface ICountriesList{
-    countriesList: ISingleCountry[],
-    pagination: string;
-    themeColor: string;
-    switchThemeColor: () => void;
+interface ICountriesList {
+  countriesList: ISingleCountry[];
+  pagination: string;
+  themeColor: string;
+  switchThemeColor: () => void;
 }
-interface IThemeColor  {
-    themeColor: string;
+interface IThemeColor {
+  themeColor: string;
 }
-export const SearchAndContent:React.FC<ICountriesList> = (props)=>{
-    const [countriesList, setCountriesList] = useState<ISingleCountry[]>([])
-    const [region, setRegion] = useState<string>('Region')
-    const [pagesNumber, setPagesNumber] = useState<number>(0)
-    const [choosePageNumber, setchoosePageNumber] = useState<number>(1)
-    const pagesNumberEl = useRef<any>([])
-    let  filteredCountries:ISingleCountry[] = []
-    let history = useHistory();
+export const SearchAndContent: React.FC<ICountriesList> = (props) => {
+  const [countriesList, setCountriesList] = useState<ISingleCountry[]>([]);
+  const [region, setRegion] = useState<string>("Region");
+  const [pagesNumber, setPagesNumber] = useState<number>(0);
+  const [choosePageNumber, setchoosePageNumber] = useState<number>(1);
+  const pagesNumberEl = useRef<any>([]);
+  let filteredCountries: ISingleCountry[] = [];
+  let history = useHistory();
 
+  const handleMoreInfo = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    const countryRegion = (e.target as Element).parentElement?.parentElement
+      ?.parentElement?.dataset.region;
+    const countryName = (e.target as Element).parentElement?.parentElement
+      ?.parentElement?.dataset.name;
 
+    const country = countriesList.filter((country: ISingleCountry) => {
+      if (country.name === countryName && country.region === countryRegion)
+        return country;
+    });
+    history.push({
+      pathname: `/${countryRegion}/${countryName}`,
+      state: { country },
+    });
+  };
+  const selectRegion = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRegion(e.target.value);
+    setchoosePageNumber(1);
+    pagesNumberEl.current.forEach((item: any) => {
+      if (item.current === null) return;
+      props.themeColor === "light"
+        ? (item.current.style.backgroundColor = "#fff")
+        : (item.current.style.backgroundColor = "#1E2C35");
+      if (item.current.dataset.number === "1")
+        props.themeColor === "light"
+          ? (item.current.style.backgroundColor = "lightgrey")
+          : (item.current.style.backgroundColor = "#0a3855");
+    });
+  };
+  const handleChoosePageNumber = (e: any) => {
+    setchoosePageNumber(e.target.dataset.number * 1);
 
+    pagesNumberEl.current.forEach((item: any) => {
+      if (item.current === null) return;
+      props.themeColor === "light"
+        ? (item.current.style.backgroundColor = "#fff")
+        : (item.current.style.backgroundColor = "#1E2C35");
 
-    const handleMoreInfo = (e:React.MouseEvent<HTMLAnchorElement, MouseEvent>)=>{
-       
-        const countryRegion = (e.target as Element).parentElement?.parentElement?.parentElement?.dataset.region;
-        const countryName = (e.target as Element).parentElement?.parentElement?.parentElement?.dataset.name
-        
-        const country = countriesList.filter((country:ISingleCountry)=>{
-            if(country.name===countryName && country.region===countryRegion)
-                return country
-        })
-        history.push({
-            pathname: `/${countryRegion}/${countryName}`,
-            state: {country}
-        })
-    }
-    const selectRegion = (e:React.ChangeEvent<HTMLSelectElement>)=>{
-        setRegion(e.target.value)
-        setchoosePageNumber(1)
-        pagesNumberEl.current.forEach((item:any)=>{
-            if(item.current === null)
-            return
-            props.themeColor==="light" ?  item.current.style.backgroundColor = "#fff" : item.current.style.backgroundColor = "#1E2C35"
-            if(item.current.dataset.number === "1")
-                props.themeColor==="light" ? item.current.style.backgroundColor = "lightgrey" : item.current.style.backgroundColor = "#0a3855"
-        })
-    }
-    const handleChoosePageNumber = (e: any) =>{
-        setchoosePageNumber(e.target.dataset.number * 1)
-
-        pagesNumberEl.current.forEach((item:any)=>{
-            if(item.current === null)
-            return
-            props.themeColor==="light" ?  item.current.style.backgroundColor = "#fff" : item.current.style.backgroundColor = "#1E2C35"
-            
-            if(item.current.dataset.number === e.target.dataset.number){
-            props.themeColor==="light" ? item.current.style.backgroundColor = "lightgrey" : item.current.style.backgroundColor = "#0a3855"            
-            }
-        })
-    }
-
-  
-    useEffect(()=>{
-        
-        if(region==='Region'){
-            setCountriesList(props.countriesList)
-            setPagesNumber(Math.ceil(props.countriesList.length / 10))
-        }
-        else{
-            const countriesByregion = props.countriesList.filter((country: ISingleCountry) =>{
-                
-                if(country.region=== region){
-                    return country
-                }        
-            })
-            setCountriesList(countriesByregion)
-            setPagesNumber(Math.ceil(countriesByregion.length / 10))
-        }
-        
-    }, [region, props.countriesList])
-    
-    const searchCountries = (e: React.ChangeEvent<HTMLInputElement>)=>{
-        pagesNumberEl.current.forEach((item:any)=>{
-            if(item.current === null)
-            return
-            props.themeColor==="light" ?  item.current.style.backgroundColor = "#fff" : item.current.style.backgroundColor = "#1E2C35"
-            if(item.current.dataset.number === "1")
-            props.themeColor==="light" ? item.current.style.backgroundColor = "lightgrey" : item.current.style.backgroundColor = "#0a3855"
-        })
-        
-        setchoosePageNumber(1)
-       filteredCountries = props.countriesList.filter((country: ISingleCountry, index: number)=>{
-            
-            if(country.name.includes(`${e.target.value}`)){
-                if(country.region===region)
-                return country;
-                else if(region==='Region')
-                return country
-            }
-        })
-      
-        setCountriesList(filteredCountries)
-        setPagesNumber(Math.ceil(filteredCountries.length/10))
-       
-    }
-    const incerementPageNumber = () =>{
-        if(choosePageNumber>=Math.ceil(countriesList.length)/10){
-            return;
-        }
-        setchoosePageNumber(choosePageNumber+1)
-    }
-    const decrementPageNumber = () => {
-        if(choosePageNumber<=1){
-            return;
-        }
-        setchoosePageNumber(choosePageNumber-1)
-    }
-    
-   
-   
-    if (pagesNumberEl.current.length !== (Math.ceil(countriesList.length)/10)) {
-        pagesNumberEl.current = Array(25).fill(0).map((_, i) => pagesNumberEl.current[i] || createRef());
+      if (item.current.dataset.number === e.target.dataset.number) {
+        props.themeColor === "light"
+          ? (item.current.style.backgroundColor = "lightgrey")
+          : (item.current.style.backgroundColor = "#0a3855");
       }
+    });
+  };
 
-      
-    
-    return(
-        <>
-        <SearchRegionWrapper>
-            <SearchInput searchCountries = {searchCountries}/>
-            <SelectRegion selectRegion = {selectRegion}/>
-        </SearchRegionWrapper>
-        <CountriesContentWrapper>
-            <CountriesContent countriesList={countriesList} pagesNumber={choosePageNumber} handleMoreInfo={handleMoreInfo}/>
-        </CountriesContentWrapper>
-        <PaginationWrapper themeColor = {props.themeColor}>
-            <HiOutlineArrowCircleLeft onClick={()=>decrementPageNumber()} />
-                <Pagination pagesNumber = {pagesNumber} handleChoosePageNumber= {handleChoosePageNumber} pagination={props.pagination} choosePageNumber = {choosePageNumber} countriesListLength = {countriesList.length} pagesNumberEl = {pagesNumberEl} themeColor={props.themeColor} />
-            <HiOutlineArrowCircleRight onClick={()=>incerementPageNumber()} />
-        </PaginationWrapper>
-        </>
-    )
-}
+  useEffect(() => {
+    if (region === "Region") {
+      setCountriesList(props.countriesList);
+      setPagesNumber(Math.ceil(props.countriesList.length / 10));
+    } else {
+      const countriesByregion = props.countriesList.filter(
+        (country: ISingleCountry) => {
+          if (country.region === region) {
+            return country;
+          }
+        }
+      );
+      setCountriesList(countriesByregion);
+      setPagesNumber(Math.ceil(countriesByregion.length / 10));
+    }
+
+    return () => {};
+  }, [region, props.countriesList]);
+
+  const searchCountries = (e: React.ChangeEvent<HTMLInputElement>) => {
+    pagesNumberEl.current.forEach((item: any) => {
+      if (item.current === null) return;
+      props.themeColor === "light"
+        ? (item.current.style.backgroundColor = "#fff")
+        : (item.current.style.backgroundColor = "#1E2C35");
+      if (item.current.dataset.number === "1")
+        props.themeColor === "light"
+          ? (item.current.style.backgroundColor = "lightgrey")
+          : (item.current.style.backgroundColor = "#0a3855");
+    });
+
+    setchoosePageNumber(1);
+    filteredCountries = props.countriesList.filter(
+      (country: ISingleCountry, index: number) => {
+        if (country.name.includes(`${e.target.value}`)) {
+          if (country.region === region) return country;
+          else if (region === "Region") return country;
+        }
+      }
+    );
+
+    setCountriesList(filteredCountries);
+    setPagesNumber(Math.ceil(filteredCountries.length / 10));
+  };
+  const incerementPageNumber = () => {
+    if (choosePageNumber >= Math.ceil(countriesList.length) / 10) {
+      return;
+    }
+    setchoosePageNumber(choosePageNumber + 1);
+  };
+  const decrementPageNumber = () => {
+    if (choosePageNumber <= 1) {
+      return;
+    }
+    setchoosePageNumber(choosePageNumber - 1);
+  };
+
+  if (pagesNumberEl.current.length !== Math.ceil(countriesList.length) / 10) {
+    pagesNumberEl.current = Array(25)
+      .fill(0)
+      .map((_, i) => pagesNumberEl.current[i] || createRef());
+  }
+
+  return (
+    <>
+      <SearchRegionWrapper>
+        <SearchInput searchCountries={searchCountries} />
+        <SelectRegion selectRegion={selectRegion} />
+      </SearchRegionWrapper>
+      <CountriesContentWrapper>
+        <CountriesContent
+          countriesList={countriesList}
+          pagesNumber={choosePageNumber}
+          handleMoreInfo={handleMoreInfo}
+        />
+      </CountriesContentWrapper>
+      <PaginationWrapper themeColor={props.themeColor}>
+        <HiOutlineArrowCircleLeft onClick={() => decrementPageNumber()} />
+        <Pagination
+          pagesNumber={pagesNumber}
+          handleChoosePageNumber={handleChoosePageNumber}
+          pagination={props.pagination}
+          choosePageNumber={choosePageNumber}
+          countriesListLength={countriesList.length}
+          pagesNumberEl={pagesNumberEl}
+          themeColor={props.themeColor}
+        />
+        <HiOutlineArrowCircleRight onClick={() => incerementPageNumber()} />
+      </PaginationWrapper>
+    </>
+  );
+};
 
 const CountriesContentWrapper = styled.div`
-box-sizing: border-box;
-width:100%;
-min-height: 10vh;
-display: flex;
-padding: 1.5rem 0 2rem 0;
+  box-sizing: border-box;
+  width: 100%;
+  min-height: 10vh;
+  display: flex;
+  padding: 1.5rem 0 2rem 0;
 
-background-color: ${props => props.theme.colors.backgroundContentCard};
-display: flex;
-flex-direction: column;
-align-items: center;
+  background-color: ${(props) => props.theme.colors.backgroundContentCard};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-@media (min-width: 768px){
-   
+  @media (min-width: 768px) {
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: stretch;
     padding-left: 1rem;
-}
-@media (min-width: 1024px){
+  }
+  @media (min-width: 1024px) {
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: stretch;
     padding-left: 1rem;
-
-}
-`
-
+  }
+`;
 
 const SearchRegionWrapper = styled.div`
-width:100%;
-min-height: 5vh;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-padding: 0.2rem 0.5rem;
+  width: 100%;
+  min-height: 5vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.2rem 0.5rem;
 
-input{
-        width: 90%;
-        padding: 12px 24px;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 2px 9px;
-        
-        transition: transform 250ms ease-in-out;
-        font-size: 14px;
-        line-height: 18px;
-        
-        color: ${props => props.theme.colors.fontColorInput};
-        background-color: ${props => props.theme.colors.inputbackground};
+  input {
+    width: 90%;
+    padding: 12px 24px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 2px 9px;
 
- 
-      background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
-        background-repeat: no-repeat;
-        background-size: 18px 18px;
-        background-position: 95% center;
-        border-radius: 50px;
-        border: none;
-        transition: padding 250ms ease-in-out;
-        backface-visibility: hidden;
-        transform-style: preserve-3d;
-    
-        &::placeholder {
-            color: ${props => props.theme.colors.fontColorInputPlaceholder};
-            text-transform: uppercase;
-            letter-spacing: 1.5px;
-        }
-        
-        &:hover,
-        &:focus {
-            padding: 12px 12px;
-            outline: 0;
-            border: none;
-        
-            border-radius: 0;
-            background-position: 95% center;
-        }
-    
-}
+    transition: transform 250ms ease-in-out;
+    font-size: 14px;
+    line-height: 18px;
 
+    color: ${(props) => props.theme.colors.fontColorInput};
+    background-color: ${(props) => props.theme.colors.inputbackground};
 
-`
- 
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-size: 18px 18px;
+    background-position: 95% center;
+    border-radius: 50px;
+    border: none;
+    transition: padding 250ms ease-in-out;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+
+    &::placeholder {
+      color: ${(props) => props.theme.colors.fontColorInputPlaceholder};
+      text-transform: uppercase;
+      letter-spacing: 1.5px;
+    }
+
+    &:hover,
+    &:focus {
+      padding: 12px 12px;
+      outline: 0;
+      border: none;
+
+      border-radius: 0;
+      background-position: 95% center;
+    }
+  }
+`;
 
 const PaginationWrapper = styled.div<IThemeColor>`
-width: 100%;
-min-height: 2vh;
-padding:4px;
-display: flex;
-align-items: center;
-justify-content: center;
-div{
+  width: 100%;
+  min-height: 2vh;
+  padding: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
     margin: 0.4rem;
     display: flex;
     align-items: center;
@@ -250,33 +261,34 @@ div{
     border-radius: 50%;
     width: 30px;
     height: 30px;
-    border: 0.1rem solid ${props => props.themeColor === "light" ? "lightgray" : "#fff"};
-    color: ${props => props.themeColor === "light" ? "#000" : "#fff"};
-
-}
-div:nth-of-type(1){
-    background-color: ${props => props.themeColor === "light" ? "#fff" : "#1E2C35"};
-    color: ${props => props.themeColor === "light" ? "#000" : "#fff"};  
-}
-svg{
+    border: 0.1rem solid
+      ${(props) => (props.themeColor === "light" ? "lightgray" : "#fff")};
+    color: ${(props) => (props.themeColor === "light" ? "#000" : "#fff")};
+  }
+  div:nth-of-type(1) {
+    background-color: ${(props) =>
+      props.themeColor === "light" ? "#fff" : "#1E2C35"};
+    color: ${(props) => (props.themeColor === "light" ? "#000" : "#fff")};
+  }
+  svg {
     display: block;
     width: 30px;
     height: 30px;
-}
-svg:nth-of-type(1){
+    cursor: pointer;
+  }
+  svg:nth-of-type(1) {
     margin-right: 0.3rem;
-}
-svg:nth-of-type(2){
+  }
+  svg:nth-of-type(2) {
     margin-left: 0.3rem;
-}
-@media (min-width: 1023px){
+  }
+  @media (min-width: 1023px) {
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
     padding-left: 1rem;
-    svg{
-        display: none;
+    svg {
+      display: none;
     }
-
-}
-`
+  }
+`;
